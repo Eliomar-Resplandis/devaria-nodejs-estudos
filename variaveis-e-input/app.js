c4aaf968dbde73981c265bab0f1303450e4b275d
Eliomar-Resplandis <100532243+Eliomar-Resplandis@users.noreply.github.com>
2022-03-16 16:56:45 -0400

    Variáveis e input

--- variaveis-e-input/app.js
+++ variaveis-e-input/app.js
@@ -1,2 +1,13 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const minhaPrimeiraContanteString = 'Minha primeira constante' ;
 console.log(minhaPrimeiraContanteString);
+
+let leituraDeCampo;
+readline.question('Por favor, digite alguma coisa:', (input) => {
+    leituraDeCampo = input;
+    console.log(`O usuário digitou ${leituraDeCampo}`);
+});
